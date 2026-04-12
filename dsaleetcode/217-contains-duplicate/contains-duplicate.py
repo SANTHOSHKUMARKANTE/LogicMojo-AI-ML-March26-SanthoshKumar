174560e5bd48025a3e7e94cee4563d70f5a07c0c
SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2026-04-12 12:21:15 +0530

    Time: 13 ms (47.08%) | Memory: 32.4 MB (29.11%) - LeetSync

--- dsaleetcode/217-contains-duplicate/contains-duplicate.py
+++ dsaleetcode/217-contains-duplicate/contains-duplicate.py
@@ -1,17 +1,31 @@
 # As a begineer this is my solution
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        dici = {}
 
-        for key in nums:
-            dici[key] = dici.get(key,0) + 1
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         dici = {}
 
-        res = [True for val in dici.values() if val > 1]
-        
-        if(True in res):
-            return True
-        return False
+#         for key in nums:
+#             dici[key] = dici.get(key, 0) + 1
+
+#         res = [True for val in dici.values() if val > 1]
+
+#         if True in res:
+#             return True
+#         return False
 
 
 # solution of precise and optimsed
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seek = set()
+
+        for key in nums:
+            if key in seek:
+                return True
+            else:
+                seek.add(key)
+        return False
+
+        
