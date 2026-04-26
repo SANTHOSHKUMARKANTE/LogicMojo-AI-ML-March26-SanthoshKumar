9497d8b854b2b0d63cfd49a8d558d65bc92fe820
SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2026-04-26 13:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (47.76%) - LeetSync

--- dsaleetcode/349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ dsaleetcode/349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,19 +1,29 @@
+#as a begineer this is my solution
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         if(len(nums1) > len(nums2)):
+#             res = []
+#             size = len(nums2)
+
+#             for i in nums2:
+#                 if i in nums1 and i not in res:
+
+#                     res.append(i)
+#         else:
+#             res = []
+#             size = len(nums1)
+
+#             for i in nums1:
+#                 if i in nums2 and i not in res:
+#                     res.append(i)
+#         return res
+# Optimal solution
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if(len(nums1) > len(nums2)):
-            res = []
-            size = len(nums2)
+     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
-            for i in nums2:
-                if i in nums1 and i not in res:
+        #remove duplicates in the lists bu converting into the sets
 
-                    res.append(i)
-        else:
-            res = []
-            size = len(nums1)
+        set1 = set(nums1)
+        set2 = set(nums2)
 
-            for i in nums1:
-                if i in nums2 and i not in res:
-                    res.append(i)
-        return res
-                
+        return list(set1 & set2)
