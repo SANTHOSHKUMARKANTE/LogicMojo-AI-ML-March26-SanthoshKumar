0dafdd478225f4e6f674c2c38854324270a46f25
b160user08 <b160user08@rpsconsulting,in>
2026-06-01 22:08:34 +0530

    feat: initial commit

---
+++ Practice/projects/mlproject1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_DOT= '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of the requirements
+    '''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_DOT in requirements:
+            requirements.remove(HYPHEN_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='santhosh',
+    author_email='kantesanthun123@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
