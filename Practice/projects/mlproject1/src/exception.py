90aa1f8c35a0bf495be54702c037bb1cf95a61ab
b160user08 <b160user08@rpsconsulting,in>
2026-06-06 11:52:30 +0530

    exception modified

--- Practice/projects/mlproject1/src/exception.py
+++ Practice/projects/mlproject1/src/exception.py
@@ -1,5 +1,8 @@
 import sys
 import logging
+from logger import logging
+
+
 def error_message_details(error, error_detail:sys):
   _,_,exc_tb=error_detail.exc_info()
   file_name=exc_tb.tb_frame.f_code.co_filename
@@ -19,5 +22,10 @@ class CustomException(Exception):
   def __str__(self):
     return self.error_message
   
- 
+# if __name__ == "__main__":
+#   try:
+#     a=1/0
+#   except Exception as e:
+#     logging.error("Divide by zero")
+#     raise CustomException(e, sys)
     
